216d9866e896d863088a4c57b01ed3430d83c91e
TimLym <january26148@gmail.com>
2025-08-12 22:49:14 +0800

    元件裡拆掉card層(與card-body重疊)

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,20 +1,18 @@
 import "../css/app0811-weather.css"
 import { CiUmbrella } from "react-icons/ci";
 
-const WeatherCard = ({date, timeStart, comma, timeEnd, weatherImgUrl, status, rainRate}) => {
+const WeatherCard = ({ date, timeStart, comma, timeEnd, weatherImgUrl, status, rainRate }) => {
   return (
-    <div className="weather-card">
-      <div className="card-body">
-          <h3 className="card-date">{date}</h3>
-          <p className="card-time">{timeStart}</p>
-          <p className="card-time">{comma}</p>
-          <p className="card-time">{timeEnd}</p>
-          <img src={weatherImgUrl} className="card-img" alt="..." />
-          <p className="card-status">{status}</p>
-          <div className="rain-rate-sec">
-              <CiUmbrella />
-              <p className="rain-rate">{rainRate}%</p>
-          </div>
+    <div className="card-body">
+      <h3 className="card-date">{date}</h3>
+      <p className="card-time">{timeStart}</p>
+      <p className="card-time">{comma}</p>
+      <p className="card-time">{timeEnd}</p>
+      <img src={weatherImgUrl} className="card-img" alt="..." />
+      <p className="card-status">{status}</p>
+      <div className="rain-rate-sec">
+        <CiUmbrella />
+        <p className="rain-rate">{rainRate}%</p>
       </div>
     </div>
   )
